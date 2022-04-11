b8fd041a5de255b8702c942f7d326de587b90f37
Nikolay Danailov <frostblooded@yahoo.com>
2022-04-11 14:51:43 +0300

    Make rockets rotate toward player (doesn't rotate properly yet)

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public float speed = 10;
+    public float rotationSpeed = 0.3f;
+
     private void Update()
     {
-        transform.position += Vector3.up * Time.deltaTime;
+        // transform.position += transform.up * speed * Time.deltaTime;
+
+        Transform player = FindObjectOfType<Player>().transform;
+        Vector3 vectorToTarget = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, q, Time.deltaTime * speed);
     }
 }
